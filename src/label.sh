af66e04192e5e8e29d2ed1647b17ac95e364968e
Kyle Roe <hopeman15@users.noreply.github.com>
2026-03-21 10:13:25 +0100

    fix: make keyword matching case-insensitive (#31)
    * fix: make keyword matching case-insensitive
    
    * bump version

--- src/label.sh
+++ src/label.sh
@@ -9,7 +9,7 @@ REPO="${REPO:?REPO is not set}"
 PR_TITLE=$(gh pr view "$PR_NUMBER" -R "$REPO" --json title -q '.title')
 
 "${GITHUB_ACTION_PATH}/src/parse_labels.sh" | while IFS=',' read -r keyword label; do
-  if [[ "$PR_TITLE" == *"$keyword"* ]]; then
+  if [[ "${PR_TITLE,,}" == *"${keyword,,}"* ]]; then
     echo "Adding label: $label for keyword: $keyword"
     gh pr edit "$PR_NUMBER" --add-label "$label" -R "$REPO"
   fi
